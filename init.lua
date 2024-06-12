8e077913b2614c99bc3cd442fc229a38a84b1b80
Shane Walker <swalker@lemongrasscloud.com>
2024-06-12 14:23:40 -0500

    where I left off last time I thought I wanted to switch to neovim

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Ensure the undo directory exists
+local undodir = vim.fn.expand("~/.undo/")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
+
+-- Set the undo directory
+vim.o.undodir = undodir
+vim.o.undofile = true
